4a4933df9aafed16c2c74869c2585ce355689f57
Martyna Szymańska <martix171717@gmail.com>
2022-03-28 22:41:59 +0200

    Add local time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 import getting_url
 
@@ -16,7 +16,8 @@ def temperature():
     API_Key = '386df314be53675038e1916ba83dcee8'
     base_url = "https://api.openweathermap.org/data/2.5/weather?"+"appid=" + API_Key+ "&q="+ city_name
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    utc_time = datetime.utcnow()
     r = requests.get(base_url)
     json_object = r.json()
     try:
@@ -30,11 +31,14 @@ def temperature():
         country = json_object['sys']['country']
         sunrise = time.strftime("%H:%M", time.localtime(int(json_object['sys']['sunrise'])))
         sunset = time.strftime("%H:%M", time.localtime(int(json_object['sys']['sunset'])))
+        timezone= json_object['timezone']
+        local = utc_time + timedelta(seconds=timezone)
+        local_time = local.strftime("%H:%M")
     except KeyError:
         return render_template('keyerror.html', city_name = city_name)
     return render_template('temperature.html', temp = temp, city_name = city_name, temp_feel=temp_feel, \
         wind=wind, pressure=pressure, dt_string=dt_string, hum=hum, desc=desc, cloud=cloud, country=country,\
-            sunrise = sunrise, sunset=sunset)
+            sunrise = sunrise, sunset=sunset, timezone=timezone, local=local, utc_time=utc_time, local_time=local_time)
 
 @app.context_processor
 def utility_processor():
